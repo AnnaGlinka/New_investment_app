17aacebac01776d207ce505a91406ab36262b809
AnnaGlinka <aglinka8@gmail.com>
2023-03-21 09:39:19 +0000

    Get eu_vat_rates with wrong endpoint and display 'No data available'

--- get_eu_VAT_rates.py
+++ get_eu_VAT_rates.py
@@ -13,19 +13,24 @@ class EuVATRates:
 
     def get_vat_retes(self):
         url = "https://api.exchangerate.host/vat_rates"
+        #url = "https://api.exchangerate.host/vat_ra" #error in endpoint
         EU_VAT_rates = self._APIdataG.get_API_data(url)
-    
-        for country, rate in EU_VAT_rates['rates'].items():
-            if rate['reduced_rates'] == []:
-                rate['reduced_rates'] = 'Not applicable'
-            if rate['super_reduced_rates'] == []:
-                rate['super_reduced_rates'] = 'Not applicable'
-            if rate['parking_rates'] == []:
-                rate['parking_rates'] = 'Not applicable'
+
+        try:
+            for country, rate in EU_VAT_rates['rates'].items():
+                if rate['reduced_rates'] == []:
+                    rate['reduced_rates'] = 'Not applicable'
+                if rate['super_reduced_rates'] == []:
+                    rate['super_reduced_rates'] = 'Not applicable'
+                if rate['parking_rates'] == []:
+                    rate['parking_rates'] = 'Not applicable'
+                
+            return EU_VAT_rates['rates']
             
-        return EU_VAT_rates['rates']
+        except TypeError:
+            print(f"Data not available! \nEndpoint: {url}")
 
 
-# evr = EuVATRates()
-# vat = evr.get_vat_retes()
-# print(vat)
+evr = EuVATRates()
+vat = evr.get_vat_retes()
+print(vat)
